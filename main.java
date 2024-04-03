f91ee0ad045d2c9f2e3cf59ad37e7a62b187905e
ViktoriiaBriunina <125908710+ViktoriiaBriunina@users.noreply.github.com>
2024-04-03 19:19:40 +0300

    Create main.java
    file main.java is added

---
+++ main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+  public static void main(String[] args) {
+    
+    int temp = 10;
+
+    if (temp <= 5 ) {
+      System.out.println("Wear a warm jacket!");
+    }
+    else if (season.equals("spring")) {
+      System.out.println("Wear a T-shirt!");
+    }
+    else if (season.equals("summer")) {
+      System.out.println("Wear a swimming suite!");
+    }
+    else if (season.equals("autumn")) {
+      System.out.println("Wear a rain coat!");
+    }
+    else {
+      System.out.println("I don't recognize this season!");
+    }
+  }
+
+}
